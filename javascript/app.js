a69d17286c98b1f64c1a7cb92647f56e694fde63
arpan <arpan@gmail.com>
2022-08-22 10:45:35 +1200

    contact

--- javascript/app.js
+++ javascript/app.js
@@ -2,22 +2,12 @@ const footer = document.querySelector("footer > p");
 footer.innerHTML = `All rights reserved. <br> &copy; & p ScribeCraft ${new Date().getFullYear()}`;
 
 
+$(document).ready(function() {
+    $("#bars").click(function(){
+        $("#nav__links").slideToggle();
+    })
+})
 
-const navToggle = () => {   
-
-    const navBars = document.querySelector(".bar");
-
-        navBars.addEventListener('click', function() {
-            const navContent = document.querySelector(".nav__links");
-           
-
-            if(navContent.style.display === "flex") {
-                navContent.style.display = "none";
-            }else{
-                navContent.style.display = "flex";
-            }
-        })    
-}
 
 
 const buyNow = () => {
